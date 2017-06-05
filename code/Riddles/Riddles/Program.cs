3d9dc1e79f16c2900f9544ae2128f9321db13431
Kite2901 <kite2995@gmail.com>
2017-06-05 16:28:17 +0100

    Add files via upload

---
+++ code/Riddles/Riddles/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riddles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response ="";
+            int count = 0;
+            int countCorrect = 0;
+            Console.WriteLine("Why did the chicken cross the road?");
+      
+           
+            while ((response != "to get to the other side") && (response != "because the light was green") &&(response != "i give up") && count<3)
+            {
+               response= Console.ReadLine();
+                if ((response != "to get to the other side") && (response != "because the light was green"))
+                {
+                    if (response == "i give up")
+                    {
+                        Console.WriteLine("The correct answer is To get to the other side");
+                    }
+                    else
+                        Console.WriteLine("Wrong. Try again");
+                    count++;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Well done!");
+                    countCorrect++;
+                }
+
+            } //while
+
+            Console.WriteLine("Next riddle is What's yellow and dangerous?");
+            count = 0;
+            do
+            {
+                response = Console.ReadLine();
+
+                if ((response != "shark infested custard") && (response != "shark infested lemon"))
+                {
+
+                    if (response == "i give up")
+                    {
+                        Console.WriteLine("The correct answer is shark infested custard");
+                    }
+                    else
+                        Console.WriteLine("Wrong. Try again");
+                    
+                   count++;
+                    
+
+                }
+                else
+                    Console.WriteLine("Well done!");
+                countCorrect++;
+
+
+            } while ((response != "shark infested custard") && (response != "shark infested lemon") && (response != "i give up") && count < 3);
+
+
+            Console.WriteLine("You got" + countCorrect + "correct");  //0 not showing
+
+        }  //main
+
+
+    }
+  
+}
---
+++ code/snowWhite/snowWhite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snowWhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sevenDwarves = { "Happy", "Doc", "Sleepy", "Sneezy", "Dopey", "Bashful", "Grumpy" };
+
+            for ( int i = 0; i < sevenDwarves.Length ; i++)
+            {
+
+                Console.WriteLine("Here's your dinner " + sevenDwarves[i]);
+            }
+
+            foreach (string j in sevenDwarves)
+            {
+                Console.WriteLine("Your drink is ready " + j);
+            }
+
+            for (int k = 0; k < sevenDwarves.Length; k++)
+            {
+                Console.WriteLine("What do you want for dinner " + sevenDwarves[k]);
+                string meal = Console.ReadLine();
+                Console.WriteLine(sevenDwarves[k] + "Your" + meal + "is ready!");
+            }
+
+        }
+    }
+}
